32d1e865a12bacc94266c4031ce347e0373bd25e
aryelzx <arielcearamor102@gmail.com>
2023-04-25 14:11:01 -0300

    database: add métodos de acesso

---
+++ src/shared/services/api/pessoas/PessoasServices.ts
@@ -0,0 +1,78 @@
+import { Environment } from "../../../environment"
+import { Api } from "../axios-config"
+
+interface IListagemPessoa {
+  id: number
+  nomeCompleto: string
+  email: string
+  cidadeId: number
+}
+interface IDetalhesPessoa {
+  id: number
+  nomeCompleto: string
+  email: string
+  cidadeId: number
+}
+type TPessoasComTotalCount = {
+  data: IListagemPessoa[]
+  totalCount: number
+}
+
+const getAll = async (
+  page = 1,
+  filter = ""
+): Promise<TPessoasComTotalCount | Error> => {
+  try {
+    const urlRelativa = `/pessoas?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&nomeCompleto_like=${filter}`
+
+    const { data, headers } = await Api.get(urlRelativa)
+
+    if (data) {
+      return {
+        data,
+        totalCount: headers["x-total-count"] || Environment.LIMITE_DE_LINHAS,
+      }
+    }
+    return new Error("Erro ao listar os registros.")
+  } catch (error) {
+    console.error(error)
+    return new Error(
+      (error as { message: string }).message || "Erro ao listar os registros."
+    )
+  }
+}
+//tentando fazer o getById
+const getById = async (
+  page = 1,
+  id = "",
+  filter = ""
+): Promise<IDetalhesPessoa | Error> => {
+  try {
+    const urlRelativa = `/pessoas/${id}?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&nomeCompleto_like=${filter}`
+
+    const { data } = await Api.get(urlRelativa)
+    if (data) {
+      return data
+    }
+    return new Error("Erro ao listar os registros.")
+  } catch (error) {
+    console.log(error)
+    return new Error(
+      (error as { message: string }).message || "Erro ao listar os registros."
+    )
+  }
+}
+
+const create = async (): Promise<any> => {}
+
+const updateById = async (): Promise<any> => {}
+
+const deleteById = async (): Promise<any> => {}
+
+export const PessoasServices = {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById,
+}
